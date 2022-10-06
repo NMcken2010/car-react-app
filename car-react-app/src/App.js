bbd2933e5ed2e6007e9fcd80aacaacf218157990
NMcken2010 <nmckenzie.prsvr@gmail.com>
2022-10-06 10:10:32 -0500

    first push

---
+++ car-react-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Nadia's Car App</h1>
+      <img src="https://i.ytimg.com/vi/REFhjdnIFjE/maxresdefault.jpg" alt="saleen7"/>
+    </div>
+  );
+}
+
+export default App;
